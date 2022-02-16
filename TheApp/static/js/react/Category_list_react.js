513e97c26a91a98b92518509ea7ae2d4c15df3da
unknown <george_choy@yahoo.com>
2022-02-16 14:50:20 +0800

    list categories using react

--- TheApp/static/js/react/Category_list_react.js
+++ TheApp/static/js/react/Category_list_react.js
@@ -1,10 +1,37 @@
-class Hello extends React.Component {
+const baseURL = "http://georgechoy.pythonanywhere.com/TheApp/category_list/";
+//create your react element to be rendered.
+const e = React.createElement;
+
+//your react function to do the interesting/important stuff, in this case it is to access the django rest api data
+function CategoryList2() {
+  const [post, setPost] = React.useState(null);
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post)
+   {
+    console.log(post);
+    return null;
+    }
+        const listItems = post.map((number) =>
+          <li>{number.pk} - {number.name} </li>
+        );
+  return (
+        listItems
+  );
+}
+
+//your React class to render your above function
+class CategoryListClass extends React.Component {
   render() {
-    return React.createElement('div', null, `Hello ${this.props.toWhat}`);
+      return  <CategoryList2 />;
   }
 }
 
-ReactDOM.render(
-  React.createElement(Hello, {toWhat: 'World'}, null),
-  document.getElementById('root')
-);
+//set up your container with id of the div DOM element in your html file.
+const domContainer = document.querySelector('#category_container');
+//render your React class
+ReactDOM.render(e(CategoryListClass), domContainer);
